c0e80f270dc8587ec9d4b2fa18e6aff5d057971c
Nzubechukwu <nzubennamani@gmail.com>
2019-12-06 07:02:56 +0100

    add jobs

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import Jobs from './Jobs'
 import './App.css';
 
+const mockJobs = [
+  {
+    title: "swe 1", company: "google"
+  },
+  {
+    title: "swe 1", company: "facebook"
+  },
+  {
+    title: "swe 1", company: "apple"
+  },
+  {
+    title: "swe 1", company: "microsoft"
+  }
+]
+
 function App() {
   return (
     <div className="App">
-     <Jobs />
+     <Jobs jobs={mockJobs}/>
     </div>
   );
 }
